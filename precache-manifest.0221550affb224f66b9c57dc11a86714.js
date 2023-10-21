0addcb45e7dba89560c69fea6fffcfa05ecb8701
root <root@DESKTOP-GTKK945>
2023-10-21 22:27:04 +0700

    Updates

---
+++ precache-manifest.0221550affb224f66b9c57dc11a86714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19e5124bd4b63afdde2a19b4bf33c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36591a8536f90e555f27",
+    "url": "/static/css/main.d7aa4952.chunk.css"
+  },
+  {
+    "revision": "2a8ce47eb7bfc95b1c0d",
+    "url": "/static/js/2.78186614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78186614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36591a8536f90e555f27",
+    "url": "/static/js/main.343fb137.chunk.js"
+  },
+  {
+    "revision": "b3e0eb53c9411211af37",
+    "url": "/static/js/runtime-main.20d6fe2e.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
